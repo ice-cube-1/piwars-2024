dd61b87e10f4281896f841b0c616d131f304d176
Alice Goward <alice.goward@spgs.org>
2023-12-09 20:54:39 +0000

    failed to actually commit

--- manual3.py
+++ manual3.py
@@ -1,6 +1,6 @@
 import controller
 remote=controller.initialize()
-import robot
+import robot2
 speed = 0
 steering,steeringnew = 7.5,7.5
 
@@ -8,8 +8,8 @@ def update():
     global speed,steering,steeringnew
     steeringnew = controller.get("LX",remote)
     speed = controller.get("LY",remote)
-    robot.turn(steeringnew, steering)
-    robot.forwards(speed)
+    robot2.turn(steeringnew, steering)
+    robot2.forwards(speed)
     steering = steeringnew
     
 while True:
