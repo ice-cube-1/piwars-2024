8d188996dac95a549e7e376e89e445486b84811b
ice-cube-1 <alice.goward@spgs.org>
2024-01-31 23:02:03 +0000

    more tof

--- toftest.py
+++ toftest.py
@@ -3,12 +3,49 @@ import board
 import busio
 import RPi.GPIO as GPIO
 import adafruit_vl53l0x
-XSHUT = 4
+XSHUTS = [17,4,27,22]
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(XSHUT, GPIO.OUT)
-GPIO.output(XSHUT, GPIO.HIGH)
+for i in XSHUTS:
+	GPIO.setup(i, GPIO.OUT)
+	GPIO.output(i, GPIO.HIGH)
 i2c = busio.I2C(board.SCL, board.SDA)
-vl53 = adafruit_vl53l0x.VL53L0X(i2c, address = 0x30)
-vl53.set_address(0x30)
+for i in XSHUTS:
+	GPIO.output(i, GPIO.LOW)
+
+sensors = []
+for i in range(2):
+	GPIO.output(XSHUTS[i], GPIO.HIGH)
+	sensorToChange = adafruit_vl53l0x.VL53L0X(i2c,address=0x29)
+	print("Initial value ", sensorToChange.range)
+	sensorToChange.set_address(0x30+i)
+	sensorInNewAddress = adafruit_vl53l0x.VL53L0X(i2c,address=0x30+i)
+	print("New value ", sensorInNewAddress.range)
+	sensors.append(sensorInNewAddress)
+
 while True:
-	print(vl53.range)
+	for i in range(2):
+		print(i, sensors[i].range, end='')
+		time.sleep(0.05)
+	print('')
+
+# print(sensors[0].range)
+# for i in range(1):
+# 	time.sleep(1)
+# 	print('here now',XSHUTS[i])
+# 	GPIO.output(XSHUTS[i],GPIO.HIGH)
+# 	time.sleep(1)
+# 	sensor = adafruit_vl53l0x.VL53L0X(i2c,address=0x29)
+# 	print('test')
+# 	sensors.append(sensor)
+# 	time.sleep(1)
+# 	print('hii')
+# 	# sensors[-1].set_address(0x30+i)
+# 	# sensors[-1] = adafruit_vl53l0x.VL53L0X(i2c, address=0x30+i)
+# 	GPIO.output(XSHUTS[i],GPIO.LOW)
+# 	time.sleep(1)
+# for i in XSHUTS:
+# 	GPIO.output(i, GPIO.HIGH)
+# time.sleep(1)
+
+# while True:
+# 	print(sensors[0].range)
